18a6f757f2a78519440a4da2678fc198dbbf9acc
demongki <mbahkonghariono@gmail.com>
2022-03-03 12:58:40 +0700

    friska

---
+++ ZAKKY5/listmenu.js
@@ -0,0 +1,378 @@
+exports.ownermenu = (prefix) => 
+{
+return `⌜ OWNER MENU ⌝
+𓃥 ${prefix}public
+𓃥 ${prefix}self
+𓃥 ${prefix}bc *teks*
+𓃥 ${prefix}bcg *teks*
+𓃥 ${prefix}addprem *@tag*
+𓃥 ${prefix}delprem *@tag*
+𓃥 ${prefix}listpremium
+𓃥 ${prefix}ban *@tag*
+𓃥 ${prefix}unban *@tag*
+𓃥 ${prefix}setnamabot *namanya*
+𓃥 ${prefix}setbioo *bionya*
+𓃥 ${prefix}setppbot *reply foto*
+🂡⸻`
+}
+exports.othermenu = (prefix) => 
+{
+return `⌜ OTHER ⌝
+𓃥 ${prefix}tes
+𓃥 ${prefix}runtime
+𓃥 ${prefix}owner
+𓃥 ${prefix}donasi
+𓃥 ${prefix}cekserver
+𓃥 ${prefix}tololserti
+𓃥 ${prefix}fuckboy
+𓃥 ${prefix}fuckgirl
+𓃥 ${prefix}bucinserti
+𓃥 ${prefix}pacar
+𓃥 ${prefix}serti1
+𓃥 ${prefix}serti2
+𓃥 ${prefix}serti3
+𓃥 ${prefix}jadibot
+𓃥 ${prefix}stopjadibot
+𓃥 ${prefix}listjadibot
+🂡⸻`
+}
+exports.groupmenu = (prefix) => 
+{
+return `⌜ GROUP ⌝
+𓃥 ${prefix}welcome *enable/disable*
+𓃥 ${prefix}antilink *enable/disable*
+𓃥 ${prefix}tagall *teks*
+𓃥 ${prefix}add *628××××××××*
+𓃥 ${prefix}kick *@tag*
+𓃥 ${prefix}setnamaa *Nama*
+𓃥 ${prefix}hidetag *Teks*
+𓃥 ${prefix}open
+𓃥 ${prefix}close
+𓃥 ${prefix}ganteng 
+𓃥 ${prefix}cantik 
+𓃥 ${prefix}jelek 
+𓃥 ${prefix}goblok  
+𓃥 ${prefix}bego 
+𓃥 ${prefix}pintar 
+𓃥 ${prefix}jago 
+𓃥 ${prefix}nolep 
+𓃥 ${prefix}monyet     	 
+𓃥 ${prefix}babi 
+𓃥 ${prefix}beban 
+𓃥 ${prefix}baik 
+𓃥 ${prefix}jahat 
+𓃥 ${prefix}anjing 
+𓃥 ${prefix}haram 
+𓃥 ${prefix}kontol 
+𓃥 ${prefix}pakboy 
+𓃥 ${prefix}pakgirl 
+𓃥 ${prefix}wibu 
+𓃥 ${prefix}hebat 
+𓃥 ${prefix}sadboy 
+𓃥 ${prefix}sadgirl
+🂡⸻`
+}
+exports.islamimenu = (prefix) => 
+{
+return `⌜ ISLAMI ⌝
+𓃥 ${prefix}asmaulhusna
+𓃥 ${prefix}listsurah
+𓃥 ${prefix}kisahnabi
+𓃥 ${prefix}jadwalsholat
+🂡⸻`
+}
+exports.downloadmenu = (prefix) => 
+{
+return `⌜ DOWNLOAD ⌝
+𓃥 ${prefix}play *query*
+𓃥 ${prefix}ytmp3 *link*
+𓃥 ${prefix}ytmp4 *link*
+𓃥 ${prefix}tiktok *link*
+𓃥 ${prefix}tiktoknowm *link*
+𓃥 ${prefix}tiktokwm *link*
+𓃥 ${prefix}mediafire *link*
+𓃥 ${prefix}xnxxsearch *query*
+𓃥 ${prefix}xnxxdl *link*
+𓃥 ${prefix}igvideo *link*
+𓃥 ${prefix}igfoto *link*
+𓃥 ${prefix}tiktokstalk *username*
+𓃥 ${prefix}githubstalk *username*
+𓃥 ${prefix}instagramstalk *username*
+𓃥 ${prefix}youtubestalk *username*
+🂡⸻`
+}
+exports.nulismenu = (prefix) => 
+{
+return `⌜ NULIS ⌝
+𓃥 ${prefix}nulis *teks*
+𓃥 ${prefix}nuliskanan *teks*
+𓃥 ${prefix}nuliskiri *teks*
+𓃥 ${prefix}foliokanan *teks*
+𓃥 ${prefix}foliokiri *teks*
+🂡⸻`
+}
+exports.stickermenu = (prefix) => 
+{
+return `⌜ STICKER ⌝
+𓃥 ${prefix}sticker *reply foto*
+𓃥 ${prefix}toimg *reply sticker*
+𓃥 ${prefix}tourl *reply foto*
+𓃥 ${prefix}tts *teks*
+𓃥 ${prefix}attp *teks*
+𓃥 ${prefix}ttp *teks*
+𓃥 ${prefix}ssweb *link*
+𓃥 ${prefix}sswebfull *link*
+𓃥 ${prefix}stickeranime
+𓃥 ${prefix}gura
+𓃥 ${prefix}patrick
+🂡⸻`
+}
+exports.photoxymenu = (prefix) => 
+{
+return `⌜ PHOTOXY ⌝
+𓃥 ${prefix}shadow *teks*
+𓃥 ${prefix}cup *teks*
+𓃥 ${prefix}cup1 *teks*
+𓃥 ${prefix}romance *teks*
+𓃥 ${prefix}smoke *teks*
+𓃥 ${prefix}burnpaper *teks*
+𓃥 ${prefix}lovemessage *teks*
+𓃥 ${prefix}undergrass *teks*
+𓃥 ${prefix}love *teks*
+𓃥 ${prefix}coffe *teks*
+𓃥 ${prefix}woodheart *teks*
+𓃥 ${prefix}woodenboard *teks*
+𓃥 ${prefix}summer3d *teks*
+𓃥 ${prefix}wolfmetal *teks*
+𓃥 ${prefix}nature3d *teks*
+𓃥 ${prefix}underwater *teks*
+𓃥 ${prefix}golderrose *teks*
+𓃥 ${prefix}summernature *teks*
+𓃥 ${prefix}letterleaves *teks*
+𓃥 ${prefix}glowingneon *teks*
+𓃥 ${prefix}fallleaves *teks*
+𓃥 ${prefix}flamming *teks*
+𓃥 ${prefix}harrypotter *teks*
+𓃥 ${prefix}carvedwood *teks*
+𓃥 ${prefix}tiktokmaker *teks1 teks2*
+𓃥 ${prefix}arcade8bit *teks1 teks2*
+𓃥 ${prefix}battlefield4 *teks1 teks2*
+𓃥 ${prefix}pubg *teks1 teks2*
+🂡⸻`
+}
+exports.ephotomenu = (prefix) => 
+{
+return `⌜ EPHOTO 360 ⌝
+𓃥 ${prefix}wetglass *teks*
+𓃥 ${prefix}multicolor3d *teks*
+𓃥 ${prefix}watercolor *teks*
+𓃥 ${prefix}luxurygold *teks*
+𓃥 ${prefix}galaxywallpaper *teks*
+𓃥 ${prefix}lighttext *teks*
+𓃥 ${prefix}beautifulflower *teks*
+𓃥 ${prefix}puppycute *teks*
+𓃥 ${prefix}royaltext *teks*
+𓃥 ${prefix}heartshaped *teks*
+𓃥 ${prefix}birthdaycake *teks*
+𓃥 ${prefix}galaxystyle *teks*
+𓃥 ${prefix}hologram3d *teks*
+𓃥 ${prefix}greenneon *teks*
+𓃥 ${prefix}glossychrome *teks*
+𓃥 ${prefix}greenbush *teks*
+𓃥 ${prefix}metallogo *teks*
+𓃥 ${prefix}noeltext *teks*
+𓃥 ${prefix}glittergold *teks*
+𓃥 ${prefix}textcake *teks*
+𓃥 ${prefix}starsnight *teks*
+𓃥 ${prefix}wooden3d *teks*
+𓃥 ${prefix}textbyname *teks*
+𓃥 ${prefix}writegalacy *teks*
+𓃥 ${prefix}galaxybat *teks*
+𓃥 ${prefix}snow3d *teks*
+𓃥 ${prefix}birthdayday *teks*
+𓃥 ${prefix}goldplaybutton *teks*
+𓃥 ${prefix}silverplaybutton *teks*
+𓃥 ${prefix}freefire *teks*
+𓃥 ${prefix}neondevil *teks*
+𓃥 ${prefix}3dshiny *teks*
+𓃥 ${prefix}zombie3d *teks*
+𓃥 ${prefix}wordgreen *teks*
+𓃥 ${prefix}cloud *teks*
+𓃥 ${prefix}metal *teks*
+𓃥 ${prefix}neon *teks*
+𓃥 ${prefix}graffiticolor *teks*
+𓃥 ${prefix}lightgalaxy *teks*
+𓃥 ${prefix}hotmetalic *teks*
+𓃥 ${prefix}snake *teks*
+𓃥 ${prefix}graffiti5 *teks*
+𓃥 ${prefix}graffiti3 *teks*
+𓃥 ${prefix}graffiti *teks*
+𓃥 ${prefix}neon2 *teks*
+𓃥 ${prefix}thunder *teks*
+𓃥 ${prefix}startsnight *teks*
+𓃥 ${prefix}cake *teks*
+𓃥 ${prefix}writingchalk *teks*
+𓃥 ${prefix}birthdaycake *teks*
+𓃥 ${prefix}3dhologram *teks*
+𓃥 ${prefix}galaxystyle *teks*
+𓃥 ${prefix}lighteffects *teks*
+𓃥 ${prefix}greenbrush *teks*
+𓃥 ${prefix}cakes *teks*
+𓃥 ${prefix}startsnight2 *teks*
+𓃥 ${prefix}glowing *teks*
+𓃥 ${prefix}blackpinkneon *teks*
+𓃥 ${prefix}3dcrack *teks*
+𓃥 ${prefix}3dunderwater *teks*
+𓃥 ${prefix}bearlogo *teks*
+𓃥 ${prefix}graffiti2 *teks*
+𓃥 ${prefix}watercolor *teks*
+𓃥 ${prefix}clouds *teks*
+𓃥 ${prefix}pubgmascot *teks*
+𓃥 ${prefix}summerbeach *teks*
+𓃥 ${prefix}neonlight *teks*
+𓃥 ${prefix}summerbeach2 *teks*
+𓃥 ${prefix}glow *teks*
+𓃥 ${prefix}wooden3d *teks*
+𓃥 ${prefix}galaxy *teks*
+𓃥 ${prefix}artpapercut *teks*
+𓃥 ${prefix}brokenglass *teks*
+🂡⸻`
+}
+exports.cecanmenu = (prefix) => 
+{
+return `⌜ RANDOM CECAN ⌝
+𓃥 ${prefix}cecan
+𓃥 ${prefix}cogan
+𓃥 ${prefix}china
+𓃥 ${prefix}malaysia
+𓃥 ${prefix}korea
+𓃥 ${prefix}japan
+𓃥 ${prefix}indonesia
+𓃥 ${prefix}thailand
+𓃥 ${prefix}vietnam
+𓃥 ${prefix}jeni
+𓃥 ${prefix}jiso
+𓃥 ${prefix}justina
+𓃥 ${prefix}lisa
+𓃥 ${prefix}rose
+𓃥 ${prefix}ryujin
+🂡⸻`
+}
+exports.fotomenu = (prefix) => 
+{
+return `⌜ RANDOM FOTO ⌝
+𓃥 ${prefix}darkjokes
+𓃥 ${prefix}motor
+𓃥 ${prefix}mobil
+𓃥 ${prefix}memeindo
+𓃥 ${prefix}renungan
+𓃥 ${prefix}quotesimage
+𓃥 ${prefix}katakataimage
+𓃥 ${prefix}aesthetic
+𓃥 ${prefix}yulibocil
+𓃥 ${prefix}doraemon
+𓃥 ${prefix}pokemon
+𓃥 ${prefix}pentol
+𓃥 ${prefix}tatasurya
+𓃥 ${prefix}kartun
+𓃥 ${prefix}anjing
+𓃥 ${prefix}kucing
+𓃥 ${prefix}satanic
+𓃥 ${prefix}boneka
+𓃥 ${prefix}wallpaperhacker
+𓃥 ${prefix}wallpaperhacker2
+𓃥 ${prefix}wallpaperharley
+𓃥 ${prefix}wallpaperjoker
+𓃥 ${prefix}wallpaperpubg
+𓃥 ${prefix}wallpaperhp
+𓃥 ${prefix}wallpaperhp2
+𓃥 ${prefix}wallpaperkpop
+𓃥 ${prefix}wallpaperblackpink
+𓃥 ${prefix}wallpapergame
+𓃥 ${prefix}naruto
+𓃥 ${prefix}boruto
+𓃥 ${prefix}nekonime
+𓃥 ${prefix}akira
+𓃥 ${prefix}akiyama
+𓃥 ${prefix}ana
+𓃥 ${prefix}asuna
+𓃥 ${prefix}ayuzawa
+𓃥 ${prefix}chiho
+𓃥 ${prefix}chitoge
+𓃥 ${prefix}deidara
+𓃥 ${prefix}eba
+𓃥 ${prefix}elaina
+𓃥 ${prefix}emilia
+𓃥 ${prefix}erza
+𓃥 ${prefix}gremory
+𓃥 ${prefix}hestie
+𓃥 ${prefix}hinata
+𓃥 ${prefix}isuzu
+𓃥 ${prefix}itachi
+𓃥 ${prefix}itori
+𓃥 ${prefix}kagura
+𓃥 ${prefix}kakasih
+𓃥 ${prefix}kaori
+𓃥 ${prefix}kaneki
+𓃥 ${prefix}kotori
+𓃥 ${prefix}kurumi
+𓃥 ${prefix}madara
+𓃥 ${prefix}mikasa
+𓃥 ${prefix}miku
+𓃥 ${prefix}minato
+𓃥 ${prefix}nezuko
+𓃥 ${prefix}rize
+𓃥 ${prefix}shina
+𓃥 ${prefix}shinka
+𓃥 ${prefix}shinomiya
+𓃥 ${prefix}shizuka
+𓃥 ${prefix}tejina
+𓃥 ${prefix}toukachan
+𓃥 ${prefix}tsunade
+𓃥 ${prefix}yotsuba
+𓃥 ${prefix}yuki
+𓃥 ${prefix}yumeko
+𓃥 ${prefix}fanart
+𓃥 ${prefix}nino
+🂡⸻`
+}
+exports.videomenu = (prefix) => 
+{
+return `⌜ RANDOM VIDEO ⌝
+𓃥 ${prefix}bokep
+𓃥 ${prefix}asupan
+𓃥 ${prefix}beatvn
+𓃥 ${prefix}jedagjedugff
+𓃥 ${prefix}jedagjedugml
+𓃥 ${prefix}jedagjedugpubg
+𓃥 ${prefix}storywa
+𓃥 ${prefix}storygalau
+𓃥 ${prefix}storyanime
+𓃥 ${prefix}storytruk
+𓃥 ${prefix}storybus
+𓃥 ${prefix}storysholawatan
+🂡⸻`
+}
+exports.rpgmenu = (prefix) => 
+{
+return `⌜ RPG MENU ⌝
+𓃥 ${prefix}joinrpg
+𓃥 ${prefix}quest 
+𓃥 ${prefix}mining
+𓃥 ${prefix}mancing
+𓃥 ${prefix}luckyday
+𓃥 ${prefix}luckytime
+𓃥 ${prefix}adventure
+𓃥 ${prefix}inventori
+𓃥 ${prefix}killslime
+𓃥 ${prefix}killgoblin
+𓃥 ${prefix}killdevil
+𓃥 ${prefix}killbehemoth
+𓃥 ${prefix}killdemond
+𓃥 ${prefix}killdemondking
+𓃥 ${prefix}sellikan
+𓃥 ${prefix}sellbesi
+𓃥 ${prefix}sellemas
+𓃥 ${prefix}selldiamond
+🂡⸻`
+}
